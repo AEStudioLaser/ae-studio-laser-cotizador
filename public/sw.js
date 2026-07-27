9701f0ac9e69afd7da24f63be80a95eedc32ea3c
AEStudioLaser <zeoz.ed@gmail.com>
2026-07-27 01:07:25 -0600

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,18 @@
-const CACHE='ae-stage1-v1';
-const ASSETS=['/','/logo-ae.png','/manifest.webmanifest','/icons/icon-192.png','/icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE = 'ae-studio-manager-v5'
+const CORE = ['/', '/manifest.webmanifest', '/logo-ae.png', '/icons/icon-192.png', '/icons/icon-512.png']
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)))
+  self.skipWaiting()
+})
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))))
+  self.clients.claim()
+})
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone()
+    caches.open(CACHE).then(cache => cache.put(event.request, copy))
+    return response
+  }).catch(() => caches.match(event.request).then(hit => hit || caches.match('/'))))
+})
